b487038a41226505fb9762cced4425bb2326d1c6
TangVietDien <20521185@gm.uit.edu.vn>
2022-09-30 07:51:27 +0700

    make it better

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const noiDung = {
+    "Truong:": "DH CNTT",
+    "DC": "Thu Duc"
+};
+
+const noiDung2 = {
+    "KTX:": "DHQG HCM",
+    "DC": "Thu Duc",
+    "Khu": "Khu A, Khu B"
+};
+
+app.get('/', (req, res) => {
+    console.log('Sent school information successfully');
+    res.send("hello myfriend");
+})
+
+
+app.get('/truong', (req, res) => {
+    console.log('Sent school information successfully');
+    res.send(noiDung);
+})
+
+app.get('/ktx', (req, res) => {
+    console.log('Sent dormitory information successfully');
+    res.send(noiDung2);
+});
+
+app.listen(3002, ()=>{
+    console.log('I\'m listening in port 3002');
+});
